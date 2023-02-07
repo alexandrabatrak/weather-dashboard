9470694f90faf2258fc54e34493f5d6ed50ffddb
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-02-07 00:35:42 +0000

    Add header transitions

---
+++ assets/js/header.js
@@ -0,0 +1,40 @@
+$(function () {
+  let didScroll;
+  let lastScrollTop = 0;
+  let delta = 5;
+  const header = $('header');
+  let headerHeight = header.outerHeight(true);
+  let offset = $('#forecast').offset();
+
+  $(window).scroll(function () {
+    didScroll = true;
+  });
+
+  setInterval(function () {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    let st = $(this).scrollTop();
+
+    if (st > offset.top) {
+      header.addClass('filled');
+    } else {
+      header.removeClass('filled');
+    }
+    if (Math.abs(lastScrollTop - st) <= delta) return;
+
+    if (st > lastScrollTop && st > headerHeight) {
+      header.addClass('up');
+    } else {
+      if (st + $(window).height() < $(document).height()) {
+        header.removeClass('up');
+      }
+    }
+
+    lastScrollTop = st;
+  }
+});
